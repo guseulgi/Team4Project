e56bba6563a126f5403872cbb63fe7a0e63ef982
yulimle <yulim516@naver.com>
2023-02-26 20:10:57 +0900

    230226 구조 수정

--- touristAttraction/js/main.js
+++ touristAttraction/js/main.js
@@ -1,11 +1,11 @@
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   // direction: "vertical",
-  slidesPerView: 3,
+  slidesPerView: 2,
   centeredSlides: true,
   spaceBetween: 10,
   loop: true,
-  //touchRatio: 0,
+  touchRatio: 0,
 
   // autoplay: {
   //delay: 1000,
@@ -27,13 +27,18 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-scrollbar",
   },
 });
-const imgBtn = document.querySelectorAll("img");
-const descriptContent = document.querySelectorAll(".description-content");
-const firstTop = descriptContent[0].offsetTop;
-const secondTop = descriptContent[1].offsetTop;
-const thirdTop = descriptContent[2].offsetTop;
-const fourthTop = descriptContent[3].offsetTop;
-const fifthTop = descriptContent[4].offsetTop;
+const imgBtn = document.querySelectorAll(".swiper-content");
+const descriptContent1 = document.querySelector(".description-content1");
+const descriptContent2 = document.querySelector(".description-content2");
+const descriptContent3 = document.querySelector(".description-content3");
+const descriptContent4 = document.querySelector(".description-content4");
+const descriptContent5 = document.querySelector(".description-content5");
+
+const firstTop = descriptContent1.offsetTop;
+const secondTop = descriptContent2.offsetTop;
+const thirdTop = descriptContent3.offsetTop;
+const fourthTop = descriptContent4.offsetTop;
+const fifthTop = descriptContent5.offsetTop;
 
 imgBtn[0].onclick = function () {
   window.scroll({ top: firstTop, behavior: "smooth" });
