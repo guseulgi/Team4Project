5c50e065dccac1ec31027d60d9c0537fc1930273
choiinyoung <choi121100@naver.com>
2023-02-27 15:46:05 +0900

    컨텐츠 메뉴 수정

--- food/js/food_main.js
+++ food/js/food_main.js
@@ -12,3 +12,40 @@ var mySwiper = new Swiper('.swiper-container', {
     slidesPerView: 3,  //한페이지에 보여줄 이미지의 개수
     spaceBetween: 30,  //페이지와 페이지 사이의 간격
   });
+
+  let time = document.querySelector(".time");
+  const click_li = document.querySelectorAll(".click_li");
+  // function openClick(){
+  //   time.style.display = "block"; // display 속성을 block 으로 바꾼다
+  // }
+  // click_li.addEventListener("click",function(e){
+  //   let dv = e.currentTarget;
+
+
+  // })
+
+  let before;
+
+  function toggleList(t) {
+    const parent = t.parentNode;
+    const childDiv = parent.querySelector("div");
+    
+
+    if (t !== before) {
+      for(let i = 0; i < click_li.length; i++) {
+        click_li[i].parentNode.querySelector("div").style.display = "none";
+      }
+    }
+
+    if(childDiv.style.display === "block") {
+      childDiv.style.display = "none";
+    } else {
+      childDiv.style.display = "block";
+    }
+    before = t;
+  }
+
+
+
+ 
+  
