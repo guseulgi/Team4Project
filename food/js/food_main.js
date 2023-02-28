f7852c62a8e64c425020d92dc78c80975951c7bb
choiinyoung <choi121100@naver.com>
2023-02-28 13:44:15 +0900

    두번째 페이지 수정

--- food/js/food_main.js
+++ food/js/food_main.js
@@ -30,13 +30,14 @@ var mySwiper = new Swiper('.swiper-container', {
     const parent = t.parentNode;
     const childDiv = parent.querySelector("div");
     
-
-    if (t !== before) {
+    //다른 거 눌렀을 때 안보이게
+    if (t != before) {
       for(let i = 0; i < click_li.length; i++) {
-        click_li[i].parentNode.querySelector("div").style.display = "none";
+        click_li[i].parentNode.querySelector("div").style.display = "none"; 
       }
     }
 
+    //열려있을 때 다시 누르면 닫힘
     if(childDiv.style.display === "block") {
       childDiv.style.display = "none";
     } else {
