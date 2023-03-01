0446a35ed1e64ec76dae47b0deb7b2db00029fd7
yulimle <yulim516@naver.com>
2023-03-01 15:39:39 +0900

    230301 tourist 페이지 전체적으로 수정 및 진행 중

--- touristAttraction/js/main.js
+++ touristAttraction/js/main.js
@@ -1,135 +1,3 @@
-//swiper
-let test = true;
-
-if (test) {
-  const swiper = new Swiper(".swiper", {
-    // Optional parameters
-    // direction: "vertical",
-    // slidesPerView: 2,
-    centeredSlides: true,
-    spaceBetween: 20,
-    loop: true,
-    touchRatio: 0,
-
-    autoplay: {
-      delay: 2000,
-      disableOnInteraction: false,
-    },
-
-    //If we need pagination
-    pagination: {
-      el: ".swiper-pagination",
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-      el: ".swiper-scrollbar",
-    },
-  });
-} else {
-  const swiper = new Swiper(".swiper", {
-    // Optional parameters
-    // direction: "vertical",
-    slidesPerView: 2,
-    centeredSlides: true,
-    spaceBetween: 20,
-    loop: true,
-    touchRatio: 0,
-
-    autoplay: {
-      delay: 2000,
-      disableOnInteraction: false,
-    },
-
-    //If we need pagination
-    pagination: {
-      el: ".swiper-pagination",
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-      el: ".swiper-scrollbar",
-    },
-  });
-}
-
-//scroll
-const imgBtn = document.querySelectorAll(".swiper-img");
-const descriptContent1 = document.querySelector(".description-content1");
-const descriptContent2 = document.querySelector(".description-content2");
-const descriptContent3 = document.querySelector(".description-content3");
-const descriptContent4 = document.querySelector(".description-content4");
-const descriptContent5 = document.querySelector(".description-content5");
-
-const firstTop = descriptContent1.offsetTop;
-const secondTop = descriptContent2.offsetTop;
-const thirdTop = descriptContent3.offsetTop;
-const fourthTop = descriptContent4.offsetTop;
-const fifthTop = descriptContent5.offsetTop;
-
-console.log(imgBtn);
-console.log(imgBtn[0]);
-console.log(imgBtn[1]);
-console.log(imgBtn[2]);
-console.log(imgBtn[3]);
-console.log(imgBtn[4]);
-console.log(imgBtn[5]);
-console.log(imgBtn[6]);
-
-if (test) {
-  for (let i = 0; i <= 6; i++) {
-    imgBtn[i].onclick = function () {
-      window.scroll({ top: firstTop, behavior: "smooth" });
-    };
-  }
-} else {
-  for (let i = 0; i <= 8; i++) {
-    imgBtn[i].onclick = function () {
-      window.scroll({ top: firstTop, behavior: "smooth" });
-    };
-  }
-}
-
-// imgBtn[0].onclick = function () {
-//   window.scroll({ top: firstTop, behavior: "smooth" });
-// };
-// imgBtn[5].onclick = function () {
-//   window.scroll({ top: firstTop, behavior: "smooth" });
-// };
-// imgBtn[1].onclick = function () {
-//   window.scroll({ top: secondTop, behavior: "smooth" });
-// };
-// imgBtn[6].onclick = function () {
-//   window.scroll({ top: secondTop, behavior: "smooth" });
-// };
-// imgBtn[2].onclick = function () {
-//   window.scroll({ top: thirdTop, behavior: "smooth" });
-// };
-// imgBtn[7].onclick = function () {
-//   window.scroll({ top: fifthTop, behavior: "smooth" });
-// };
-// imgBtn[3].onclick = function () {
-//   window.scroll({ top: fourthTop, behavior: "smooth" });
-// };
-// imgBtn[8].onclick = function () {
-//   window.scroll({ top: fourthTop, behavior: "smooth" });
-// };
-// imgBtn[4].onclick = function () {
-//   window.scroll({ top: fifthTop, behavior: "smooth" });
-// };
-
 // Weather API
 const weatherIconRef = {
   "01": "fas fa-sun",
@@ -190,3 +58,82 @@ fetch(weatherUrl)
   .catch((error) => {
     console.log("Weather API Error : ", error);
   });
+
+//swiper
+const swiper = new Swiper(".contents .mountainContent .swiper", {
+  // Optional parameters
+  // direction: "vertical",
+  slidesPerView: 2,
+  centeredSlides: true,
+  spaceBetween: 10,
+  loop: true,
+  // touchRatio: 0,
+
+  // autoplay: {
+  //   delay: 2000,
+  //   disableOnInteraction: false,
+  // },
+
+  //If we need pagination
+  // pagination: {
+  //   el: ".contents .swiper-pagination",
+  // },
+
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: ".contents .swiper-button-next",
+  //   prevEl: ".contents .swiper-button-prev",
+  // },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: "contents .swiper-scrollbar",
+  // },
+});
+const swiper2 = new Swiper(".contents .seaContent .swiper", {
+  // Optional parameters
+  // direction: "vertical",
+  slidesPerView: 2,
+  centeredSlides: true,
+  spaceBetween: 10,
+  // loop: true,
+});
+
+const swiperImg = document.querySelectorAll(".slide");
+const textDiv = document.querySelectorAll(".text");
+const closeDiv = document.querySelectorAll(".close");
+console.log(textDiv);
+for (let i = 0; i < swiperImg.length; i += 1) {
+  swiperImg[i].addEventListener("click", function () {
+    if (textDiv[i].classList.contains("off")) {
+      textDiv[i].classList.remove("off");
+      textDiv[i].classList.add("on");
+
+      closeDiv[i].addEventListener("click", function () {
+        if (textDiv[i].classList.contains("on")) {
+          textDiv[i].classList.remove("on");
+          textDiv[i].classList.add("off");
+        }
+      });
+    } else {
+      textDiv[i].classList.remove("on");
+      textDiv[i].classList.add("on");
+    }
+    console.log(swiperImg[0]);
+    console.log(textDiv);
+  });
+}
+//toggle
+const imgBox = document.querySelector(".mountain");
+const testContent = document.querySelector(".contents");
+console.log(imgBox);
+console.log(testContent);
+imgBox.addEventListener("click", function () {
+  if (testContent.classList.contains("hide")) {
+    testContent.classList.remove("hide");
+    imgBox.classList.add("show");
+  } else {
+    imgBox.classList.remove("show");
+    testContent.classList.add("hide");
+  }
+});
