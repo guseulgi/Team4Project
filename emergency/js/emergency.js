d8fa4d7f489795d1b220bf167af44a5f1ca0cc8d
seulgi <guseul9405@gmail.com>
2023-03-02 18:22:39 +0900

    커밋 꼬인거 정리 중입니다

--- emergency/js/emergency.js
+++ emergency/js/emergency.js
@@ -27,25 +27,49 @@ const emgSithationSwiper = new Swiper('.emgSituation .swiper-container', {
   parallax:true,
 });
 
-// 병원 정보를 담은 객체
-let hospitalObj = {
-
+// 병원 정보를 담은 클래스
+class hospitalObj {
+  constructor(call, addr, link) {
+    this.call = call;
+    this.addr = addr;
+    this.link = link;
+  }
 }
 
 let jejuHospital = {
-  '제주대학병원' : {},
-  '제주한라병원' : {},
-  '제주한국병원' : {},
-  '한마음병원' : {},
-  '서귀포 의료원' : {},
+  '제주대학병원' : new hospitalObj('064-717-1900', '제주 제주시 아란13길 15', 'https://er.jejunuh.co.kr/index.do'),
+  '제주한라병원' : new hospitalObj('064-740-5158', '제주 제주시 도령로 65', 'https://www.hallahosp.co.kr/main/'),
+  '제주한국병원' : new hospitalObj('064-750-0119', '제주 제주시 서광로 193', 'http://www.hankookhospital.co.kr'),
+  '한마음병원' : new hospitalObj('064-750-9119', '제주 제주시 연신로 52 한마음병원', ''),
+  '서귀포 의료원' : new hospitalObj('064-730-3001', '제주 서귀포시 동홍동 1530', 'http://www.smed.or.kr'),
 }
+// 지도 핀
+const mapPin = document.querySelector('.map__pin');
+
+const pinJeju = mapPin.querySelector('.pin__jeju');
+const pinHanla = mapPin.querySelector('.pin__hanla');
+const pinHanguk = mapPin.querySelector('.pin__hanguk');
+const pinHanmaeum = mapPin.querySelector('.pin__hanmaeum');
+const pinSeoguipo = mapPin.querySelector('.pin__seoguipo');
+
+// 지도 이름 읽어오기 위함
+const mapName = pinJeju.querySelector('span');
+
 
 // 브라우저 창
 const detailBrower = document.querySelector('.hospital-details');
 const navBar = detailBrower.querySelector('.nav-bar');
 const deleteBtn = navBar.querySelector('.nav-bar > li:first-child');
 
-let deleteBrower = function() {
+const detailTitle = detailBrower.querySelector('.detail-bar > .title');
+const detailCall = detailBrower.querySelector('.detail-bar .callNumber');
+const detailAddr = detailBrower.querySelector('.detail-bar .address');
+const detailLink = detailBrower.querySelector('.detail-bar .link');
+
+console.log(pinJeju.children[0].children[0].textContent);
+
+
+let pinClick = function() {
   if(detailBrower.classList.contains('d-On') === true) {
     detailBrower.classList.remove('d-On');
     detailBrower.classList.add('d-Off');
@@ -54,7 +78,13 @@ let deleteBrower = function() {
     detailBrower.classList.remove('d-Off');
   }
 }
-deleteBtn.addEventListener('click', deleteBrower);
+
+pinJeju.addEventListener('click', pinClick);
+pinHanla.addEventListener('click', pinClick);
+pinHanguk.addEventListener('click', pinClick);
+pinHanmaeum.addEventListener('click', pinClick);
+pinSeoguipo.addEventListener('click', pinClick);
+deleteBtn.addEventListener('click', pinClick);
 
 // modalBtn
 const modalBtn = navBar.querySelector('.modalBtn');
